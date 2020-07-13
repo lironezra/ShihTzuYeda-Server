29f327849fa97070fbdb2d1bfb8f9bbeb61354a5
lironezra <lironezra1@gmail.com>
2020-07-13 18:01:56 +0300

    added routes for getAllDryFoods, getById, createDryFood, delete, like, unlike

--- controllers/dryFood.js
+++ controllers/dryFood.js
@@ -1,22 +1,153 @@
 const DryFood = require('../models/DryFood');
+const cloudinary = require('cloudinary').v2;
+
+const {
+  cloudinaryApiKey,
+  cloudinaryApiSecret,
+  cloudinaryCloudName
+} = require('../config.js');
+
+cloudinary.config({
+  cloud_name: cloudinaryCloudName,
+  api_key: cloudinaryApiKey,
+  api_secret: cloudinaryApiSecret
+});
 
 module.exports = {
-  addDryFood: async (req, res) => {
-    const { imageUrl, productInfo, company, protein, lifeStage } = req.body;
-
-    const newDryFood = new DryFood({
-      imageUrl,
-      productInfo,
-      company,
-      protein,
-      lifeStage
-    });
+  addDryFood: (req, res) => {
+    const { productInfo, company, protein, lifeStage, imagePath } = req.body;
 
     try {
-      await newDryFood.save();
+      // Image upload
+      cloudinary.uploader
+        .upload(imagePath, { folder: 'shih-tzu-yeda/dryFood' })
+        .then(async (image) => {
+          const newDryFood = new DryFood({
+            imageUrl: image.url,
+            imagePublicId: image.public_id,
+            productInfo,
+            company,
+            protein,
+            lifeStage
+          });
 
-      res.status(200).json(newDryFood);
-    } catch (error) {
+          await newDryFood.save();
+
+          res.status(200).json(newDryFood);
+        })
+        .catch((err) => {
+          console.warn(err);
+          res.status(500).send('Cloaudinary uploader error: ', err);
+        });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  },
+  getAllDryFoods: async (req, res) => {
+    try {
+      const foods = await DryFood.find().sort('company');
+
+      res.status(200).send(foods);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  },
+  getDryFodById: async (req, res) => {
+    try {
+      const food = await DryFood.findById(req.params.id);
+
+      if (!food) {
+        return res.status(400).json({ msg: 'Dry food no found' });
+      }
+
+      res.status(200).json(food);
+    } catch (err) {
+      console.error(err.message);
+
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Dry food no found' });
+      }
+
+      res.status(500).send('Server Error');
+    }
+  },
+  deleteDryFood: async (req, res) => {
+    try {
+      const food = await DryFood.findById(req.params.id);
+
+      if (!food) {
+        return res.status(400).json({ msg: 'Dry food no found' });
+      }
+
+      cloudinary.api
+        .delete_resources(food.imagePublicId)
+        .then(async (result) => {
+          console.log(result);
+          await food.remove();
+
+          res.status(200).json({ msg: 'Dry food removed' });
+        })
+        .catch((err) => {
+          console.warn(err);
+          res.status(500).send('Cloaudinary delete image error: ', err);
+        });
+    } catch (err) {
+      console.error(err.message);
+
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Dry food no found' });
+      }
+
+      res.status(500).send('Server Error');
+    }
+  },
+  likeADryFood: async (req, res) => {
+    try {
+      const food = await DryFood.findById(req.params.id);
+
+      // Check if post has already been liked
+      if (
+        food.likes.filter((like) => like.user.toString() === req.user.id)
+          .length > 0
+      ) {
+        return res.status(400).json({ msg: 'Dry food already liked' });
+      }
+
+      food.likes.unshift({ user: req.user.id });
+
+      await food.save();
+
+      res.status(200).json(food.likes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  },
+  unlikeADryFood: async (req, res) => {
+    try {
+      const food = await DryFood.findById(req.params.id);
+
+      // Check if post has already been liked
+      if (
+        food.likes.filter((like) => like.user.toString() === req.user.id)
+          .length === 0
+      ) {
+        return res.status(400).json({ msg: 'Dry food already liked' });
+      }
+
+      // Get the remove index
+      const removeIdex = food.likes
+        .map((like) => like.user.toString())
+        .indexOf(req.user.id);
+
+      food.likes.splice(removeIdex, 1);
+
+      await food.save();
+
+      res.status(200).json(food.likes);
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
     }
--- routes/api/dryfood.js
+++ routes/api/dryfood.js
@@ -6,8 +6,8 @@ const DryFoodController = require('../../controllers/dryFood');
 const passportConf = require('../../passport'); // loading passport configuration
 const passportJWT = passport.authenticate('jwt', { session: false });
 
-// @route   PUT api/dryfood/
-// @desc    Add dry food item
+// @route   PUT api/dryfood
+// @desc    Craete dry food item
 // @access  Private
 router
   .route('/')
@@ -16,5 +16,30 @@ router
     passportJWT,
     DryFoodController.addDryFood
   );
+
+// @route   GET api/dryfood
+// @desc    Get all dry food items
+// @access  Private
+router.route('/').get(passportJWT, DryFoodController.getAllDryFoods);
+
+// @route   GET api/dryfood/:id
+// @desc    Get food by id
+// @access  Private
+router.route('/:id').get(passportJWT, DryFoodController.getDryFodById);
+
+// @route   DELETE api/dryfood/:id
+// @desc    Delete dry food
+// @access  Private
+router.route('/:id').delete(passportJWT, DryFoodController.deleteDryFood);
+
+// @route   PUT api/dryfood/like/:id
+// @desc    Like a dry food
+// @access  Private
+router.route('/like/:id').put(passportJWT, DryFoodController.likeADryFood);
+
+// @route   PUT api/dryfood/unlike/:id
+// @desc    Unlike a dry food
+// @access  Private
+router.route('/unlike/:id').put(passportJWT, DryFoodController.unlikeADryFood);
 
 module.exports = router;
